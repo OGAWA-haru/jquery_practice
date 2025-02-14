c2d819077b702f92010c2ceb3a67f176dbe91d1c
OGAWA-haru <ocohachan1004@gmail.com>
2025-02-14 23:53:36 +0900

    0214_2353_修正

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -10,24 +10,30 @@
 //   displayError(err)
 // });
 
-//入力した内容をsearchWordに代入
 $(function () { //HTMLを読みこむ
-  let pageCount = 1, f = "";
-  $(".search-btn").on("click", function () { //検索ボタンをクリックしたタイミングで
-    const searchWord = $("#search-input").val(); //クリック時の処理方法を設定
-    //pageCountの初期値は1、同じ検索ワードで検索を行う場合は、pageCountに+1する。違う検索ワードの場合は1に戻す
-    if(f === searchWord){
-      pageCount++
-    } else {
-      pageCount = 1, $(".lists").empty();
+  let pageCount = 1;
+  let find = "";
+  $(".search-btn").on("click",function(){  // 検索ボタンをクリックしたタイミングで
+    const searchWord = $("#search-input").val();  //変数を定義する
+    if (searchWord !== find){ //検索ワードと前回のワードが異なる場合に
+      pageCount = 1; //ページを１にリセット
+      $(".lists").empty(); //リストをemptyで無しにする
+      find = searchWord;
+    }else{
+      pageCount++; //同じ検索ワードが使用されている場合はページ数を増やす
     }
 
     $.ajax({ //ajaxを書く ↓settings
       url: "https://ci.nii.ac.jp/books/opensearch/search?title=" //取得したいURLを入力
-      + searchWord + "&format=json&p=" + pageCount + "&count=20" //パラメータ=値を入力
-      ,method: "GET" //Webサーバーにリクエストを送る
-    }).done(function (response) { a(response['@graph']) }) //通信成功時に 取得したresponseに対する処理を行う
-    .fail(function (err) { //通信失敗時には
+      + searchWord + "&format=json&p=" + pageCount + "&count=20", //パラメータ=値を入力
+      method: "GET" //Webサーバーにリクエストを送る
+    }).done(function (response) {  //通信成功時に 取得したresponseに対する処理を行う
+      result(response['@graph'])  //結果を表示
+    }).fail(function (err) { //通信失敗時の処理
+      displayError(err); //エラー時のメッセージを表示させる処理を行う
+    })
+
+    function displayError(err) {//APIがうまく送信されなかったときのエラー
       $(".lists").empty(); //listクラスに入っている文字を空にする
       $(".message").remove(); //messageクラスを削除する
       if(err.status === 0){ //リクエストができなかった時の処理
@@ -37,27 +43,29 @@ $(function () { //HTMLを読みこむ
       }else{ //その他 予期せぬエラーである時の処理
         $(".lists").before('<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>')
       }
-    })
-  function a(searchWord) { // 検索をしたときに実行される関数
-    $(".message").remove(); //messageクラスを削除する
-    let response;
-    //1.入力した内容で検索結果が出るときに繰り返し実行する処理内容
-    0 < (null == (response = searchWord[0].items) ? void 0 : response.length) ? $.each(searchWord[0].items,
-      function (h, c) { //以下関数を実行
-      //↓ lists-itemクラスを追加し
-      // テキストを追加＋代入された変数の中にタイトルが存在する場合はそのタイトルを、存在しない場合は「タイトル不明」を表示
-      let contents = '<li class="lists-item"><div class="list-inner"><p>タイトル：' + (c.title ? c.title : "タイトル不明")+'</p></div>';
-      // テキストを追加＋代入された変数の中に作者情報が存在する場合はその内容を、存在しない場合は「作者不明」を表示
-      contents += '<div class="list-inner"><p>作者：' + (c["dc:creator"] ? c["dc:creator"] : "作者不明")+'</p></div>';
-      // テキストを追加＋代入された変数の中に出版者情報が存在する場合はその内容を、存在しない場合は「出版者不明」を表示
-      contents += '<div class="list-inner"><p>出版者：' + (c["dc:publisher"] ? c["dc:publisher"][0] : "出版者不明")+'</p></div>';
-      // 書誌詳細ページのパーマリンクをテキスト押下時にとべるように埋め込み
-      contents += '<div class="list-inner"><a href="' + (c.link["@id"] + '"target="_blank">書籍情報</a></div></li>');
-    //2.入力した内容で検索結果が見つからなかった時に表示する処理
-    $(".lists").append(contents).css(".lists-item")}) :
-    $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>')
+    }
+
+  function result(searchWord){
+    $(".message").remove(); //messegeクラス要素を削除する
+    let response = searchWord[0].items;  //変数responseを定義
+    null == response ? void 0 : response.length; //変数responseの値がない時には必ずundefindを表示させ、値があるときにはHTML要素に対して繰り返し処理を行う
+    if(0 < void 0 || response.length ){  //検索結果が１つ以上あったときに以下の繰り返し処理を実施
+      $.each(response,function(index,item){
+        const listItem = `<li class="lists-item">
+            <div class="list-inner">
+              <p>タイトル：${item.title || "タイトル不明"}</p>
+              <p>著者：${item["dc:creator"] || "著者不明"}</p>
+              <p>出版社：${item["dc:publisher"] ? item["dc:publisher"][0] : "出版社不明"}</p>
+              <a href="${item.link["@id"]}" target="_blank">書籍情報</a>
+            </div>
+          </li>`;
+          $(".lists").prepend(listItem);
+      });
+    }else{  //検索結果が１つもなかった場合メッセージを表示
+      $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>');
+    }
   }
-  });
+});
 //リセットボタンの機能実装
   $(function(){
     $(".reset-btn").on("click", function () { //reset-btnクラスをクリックしたタイミングで
@@ -66,4 +74,4 @@ $(function () { //HTMLを読みこむ
       $("#search-input").val("") //input要素にテキスト「検索ワード」をセット
     })
   });
-  })
+})
